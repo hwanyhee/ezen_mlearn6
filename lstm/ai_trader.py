7dfed3f4d9bd9e7060cc60c16f03af1c09853f0d
hwanyhee <hwanyhee@naver.com>
2019-09-28 16:23:31 +0900

    16:24

--- lstm/ai_trader.py
+++ lstm/ai_trader.py
@@ -7,9 +7,17 @@ import pandas_datareader as data_reader
 from collections import deque
 from tqdm import tqdm
 
-class AiTraider:
-    def __init__(self):
-        pass
+class AiTraider:#에이전트 만듬
+    def __init__(self,state_size,action_space=3,model_name="AITRADER"):
+        self.state_size=state_size
+        self.action_space = action_space
+        self.model_name=model_name
+        self.memory = deque(maxlen=2000)
+        self.gamma=0.95
+        self.epsilon =1.0
+        self.epsilon_final=0.01
+        self.epsilon_decay =0.995
+        self.model = self.model_builder()
 
     def model_builder(self):
         model = tf.keras.models.Sequential()
@@ -30,8 +38,94 @@ class AiTraider:
         batch = []
         for i in range(len(self.memory) - batch_size +1, len(self.memory)):
             batch.append(self.memory[i])
+        #MDP (마르코프 디시전 프로세스)
+        for state,action,reward,next_state,done in batch:
+            reward=reward
+            if not done:
+                reward = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+            target = self.model.predict(state)
+            target[0][action]=reward
+            self.model.fit(state,target,epochs=1,verbose=0)
+        if self.epsilon > self.epsilon_final:
+            self.epsilon *=self.epsilon_decay
 
+class Trading:#환경
+    def __init__(self):
+        pass
 
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1+math.exp(-x))
+
+    @staticmethod
+    def stock_price_format(n):
+        if n < 0:
+            return "- ${0:2f}".format(abs(n))
+        else:
+            return "${0:2f}".format(abs(n))
+    @staticmethod
+    def dataset_loader(stock_name):
+        dataset= data_reader.DataReader(stock_name,data_source='yahoo')
+        start_date =str(dataset.index[0]).split()[0]
+        end_date = str(dataset.index[-1]).split()[0]
+        close =dataset['Close']
+        return close
+
+    def state_creator(self,data,timestep,window_size):
+        starting_id=timestep-window_size+1
+        if starting_id >=0:
+            windowed_data = data[starting_id:timestep+1]
+        else:
+            windowed_data = - starting_id*[data[0]]+list(data[0:timestep+1])
+
+        state=[]
+        for i in range(window_size-1):
+            state.append(self.sigmoid(windowed_data[i+1]-windowed_data[i]))
+        return np.array([state])
+
+    def transation(self,target):
+        stock_name= target
+        data = self.dataset_loader(stock_name)
+        window_size=10
+        episodes=1000
+        batch_size=32
+        data_samples =len(data)-1
+        trader =AiTraider(window_size)
+        print('========= Model Summery=============')
+        print(trader.model.summary())
+        for episode in range(1,episodes+1):
+            print('EPISODE: {}/{}'.format(episode, episodes))
+            state = self.state_creator(data, 0, window_size + 1)
+            total_profit =0 #reward
+            trader.inventory=[]
+            for t in tqdm(range(data_samples)):
+                action = trader.trade(state)
+                next_state=self.state_creator(data,t+1,window_size+1)
+                reward=0
+                if action ==1:#Buying
+                    trader.inventory.append(data[t])
+                    print('AI트레이더의 매수:',self.stock_price_format(data[t]))
+                elif action==2 and len(trader.inventory) > 0:#Selling
+                    buy_price = trader.inventory.pop(0)
+                    reward=max(data[t] - buy_price,0)
+                    total_profit +=data[t] -buy_price
+                    print('AI트레이더의 매도:',self.stock_price_format(data[t]),',이익:'+self.stock_price_format(data[t]-buy_price))
+                    if t == data_samples-1:
+                        done=True
+                    else:
+                        done =False
+                    trader.memory.append((state,action,reward,next_state,done))
+                    state=next_state
+                    if done:
+                        print('##############################')
+                        print('총 이익:{}',total_profit)
+                        print('##############################')
+                    if len(trader.memory) > batch_size:
+                        trader.batch_train(batch_size)
+
+                    if episode % 10 ==0:
+                        trader.model.save('ai_trader_{}.h5'.format(episode))
 
 if __name__ == '__main__':
-    pass
+    t = Trading()
+    t.transation('AAPL')
