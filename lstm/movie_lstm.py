9f8f7a8a1892553fd80f5f0a9fad7e71e7c50181
hwanyhee <hwanyhee@naver.com>
2019-09-28 14:57:47 +0900

    14:54

---
+++ lstm/movie_lstm.py
@@ -0,0 +1,42 @@
+from keras import layers,models,datasets
+from keras.preprocessing import sequence
+
+#데이타 생성
+class Data:
+    def __init__(self,max_features=20000,maxlen=80):
+        (x_train, y_train), (x_test, y_test) = datasets.imdb.load_data(
+            num_words=max_features
+        )
+        x_train =sequence.pad_sequences(x_train,maxlen=maxlen)
+        x_test =sequence.pad_sequences(x_test,maxlen=maxlen)
+        self.x_train,self.y_train = x_train,y_train
+        self.x_test,self.y_test = x_test,y_test
+
+#모델 생성
+class RNN_LSTM(models.Model):
+    def __init__(self,max_features,maxlen):
+        x=layers.Input((maxlen,))
+        h = layers.Embedding(max_features,128)(x)#rnn을 만듬
+        h= layers.LSTM(128,dropout=0.2,recurrent_dropout=0.2)(h)
+
+        y=layers.Dense(1,activation='sigmoid')(h)
+        super().__init__(x,y)
+        self.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+#모델을 훈련:ML
+class Machine:
+    def __init__(self,max_features=20000,maxlen=80):
+        self.data = Data(max_features,maxlen)
+        self.model = RNN_LSTM(max_features,maxlen)
+
+    def run(self,epochs=3,batch_size=32):
+        data=self.data
+        model = self.model
+        print('Traing data')
+        model.fit(data.x_train,data.y_train,batch_size=batch_size,epochs=epochs,validation_data=(data.x_test,data.y_test),verbose=2)
+        loss,acc = model.evaluate(data.x_test,data.y_test,batch_size=batch_size,verbose=2)
+        print('Tset performance: accuracy={},loss={}'.format(acc,loss))
+
+if __name__ == '__main__':
+    m= Machine()
+    m.run()
